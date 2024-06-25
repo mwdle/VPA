ba999e62f92b39ca2989adb3cc46475f647f6e98
Michael Wadley <michael@mwdle.com>
2024-06-25 16:33:51 -0600

    Fixed error

--- public/script.js
+++ public/script.js
@@ -216,7 +216,7 @@ function sendPixelChangeToServer(cellx, celly) {
  * https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
  */
 function computeIntegerPointsOnLine(x0, y0, x1, y1) {
-  x1 = Math.round(x1); y1 = Math.round(y1); x2 = Math.round(x2); y2 = Math.round(y2);
+  x0 = Math.round(x0); y0 = Math.round(y0); x1 = Math.round(x1); y1 = Math.round(y1);
   let points = [];
   let dx = Math.abs(x1 - x0);
   let dy = -Math.abs(y1 - y0);
